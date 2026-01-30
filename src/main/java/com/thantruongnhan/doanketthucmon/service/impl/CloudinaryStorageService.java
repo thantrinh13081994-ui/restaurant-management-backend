57cdec0db67373dd0e416a79b33aee25b95f8a08
thantruongnhan <thantrinh.13081994@gmail.com>
2026-01-30 23:40:59 +0700

    update

--- src/main/java/com/thantruongnhan/doanketthucmon/service/impl/CloudinaryStorageService.java
+++ src/main/java/com/thantruongnhan/doanketthucmon/service/impl/CloudinaryStorageService.java
@@ -1,15 +1,17 @@
 package com.thantruongnhan.doanketthucmon.service.impl;
 
 import com.cloudinary.Cloudinary;
-import com.cloudinary.utils.ObjectUtils;
 import com.thantruongnhan.doanketthucmon.service.StorageService;
 import lombok.RequiredArgsConstructor;
+
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
-
+import java.io.IOException;
 import java.util.Map;
 
 @Service
+@Primary
 @RequiredArgsConstructor
 public class CloudinaryStorageService implements StorageService {
 
@@ -20,13 +22,12 @@ public class CloudinaryStorageService implements StorageService {
         try {
             Map uploadResult = cloudinary.uploader().upload(
                     file.getBytes(),
-                    ObjectUtils.asMap(
-                            "folder", "products", // tên folder trên Cloudinary
-                            "resource_type", "image"));
+                    Map.of("folder", "products"));
 
             return uploadResult.get("secure_url").toString();
-        } catch (Exception e) {
-            throw new RuntimeException("Upload image to Cloudinary failed", e);
+
+        } catch (IOException e) {
+            throw new RuntimeException("Upload image failed", e);
         }
     }
 }
